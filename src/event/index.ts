8a0f23fa97b9bc97df6e92842ba8a696850d7b93
Iroha1024 <47475495+Iroha1024@users.noreply.github.com>
2021-08-28 12:04:58 +0800

    refactor: event pool

--- src/event/index.ts
+++ src/event/index.ts
@@ -1,49 +1,18 @@
 import type { ShapeProxy } from '../shape'
-import { Shape } from '../shape'
 import type { EmitEventName } from '../operation'
 
 export class EventPool {
-  private list: EventProxy[] = []
-  private cacheFlag = false
-  private cacheList: EventProxy[] = []
   private map = new Map<ShapeProxy, EventProxy>()
 
   add(shapeProxy: ShapeProxy, eventProxyConfig?: RegisterEventConfig) {
     if (!eventProxyConfig || Object.keys(eventProxyConfig).length == 0) return
-    if (this.has(shapeProxy)) return
+    if (this.map.has(shapeProxy)) return
     const eventProxy = new EventProxy(shapeProxy, eventProxyConfig)
     this.map.set(shapeProxy, eventProxy)
-    this.list.push(eventProxy)
-    this.cacheFlag = true
   }
 
   remove(shapeProxy: ShapeProxy) {
-    if (!this.has(shapeProxy)) return false
-    const index = this.findIndex(shapeProxy)
-    this.list.splice(index, 1)
-    this.map.delete(shapeProxy)
-    this.cacheFlag = true
-    return true
-  }
-
-  private findIndex(shapeProxy: ShapeProxy) {
-    return this.toList().findIndex((item) => item.shapeProxy == shapeProxy)
-  }
-
-  has(shapeProxy: ShapeProxy) {
-    return this.findIndex(shapeProxy) != -1
-  }
-
-  /**
-   * sort: layer Dsce
-   */
-  toList() {
-    if (!this.cacheFlag) return this.cacheList
-    this.cacheList = [...this.list]
-      .sort((a, b) => Shape.compare(a.shapeProxy.origin, b.shapeProxy.origin))
-      .reverse()
-    this.cacheFlag = false
-    return this.cacheList
+    return this.map.delete(shapeProxy)
   }
 
   receive(event: EmitEventName, shape: ShapeProxy) {
